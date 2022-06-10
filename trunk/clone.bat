6f48de807e2192b9f5e637ee59e3e5a8d8d913ed
adofsauron <adofsauron@163.com>
2022-06-10 18:18:05 +0800

    fix master

---
+++ trunk/clone.bat
@@ -0,0 +1,64 @@
+
+RD /s /q libfastcommon
+RD /s /q libserverframe
+RD /s /q libdiskallocator
+RD /s /q fastcfs
+RD /s /q fastdir
+RD /s /q faststore
+RD /s /q libfuse
+
+
+git rm -rf libfastcommon
+git rm -rf libserverframe
+git rm -rf libdiskallocator
+git rm -rf fastcfs
+git rm -rf fastdir
+git rm -rf faststore
+git rm -rf libfuse
+
+
+git config --global core.autocrlf false
+
+git clone https://gitee.com/fastdfs100/libfastcommon.git
+
+git clone https://gitee.com/fastdfs100/libserverframe.git
+
+git clone https://gitee.com/fastdfs100/libdiskallocator.git
+
+git clone https://gitee.com/fastdfs100/FastCFS.git
+
+git clone https://gitee.com/fastdfs100/fastDIR.git
+
+git clone https://gitee.com/fastdfs100/faststore.git
+
+git clone https://gitee.com/mirrors/libfuse.git
+
+cd libfastcommon
+RD /s /q .git
+cd ..
+
+cd libserverframe
+RD /s /q .git
+cd ..
+
+cd libdiskallocator
+RD /s /q .git
+cd ..
+
+cd fastcfs
+RD /s /q .git
+cd ..
+
+cd fastdir
+RD /s /q .git
+cd ..
+
+cd faststore
+RD /s /q .git
+cd ..
+
+cd libfuse
+RD /s /q .git
+cd ..
+
+pause
